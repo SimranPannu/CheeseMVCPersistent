235b8b574fba23083fc257b31d56cde4b247a866
Simran Pannu <simpannu14@gmail.com>
2019-12-17 16:05:34 -0500

    Finished Part3

--- src/CheeseMVC/Models/Cheese.cs
+++ src/CheeseMVC/Models/Cheese.cs
@@ -1,10 +1,19 @@
-﻿namespace CheeseMVC.Models
+﻿using System.Collections.Generic;
+
+namespace CheeseMVC.Models
 {
     public class Cheese
     {
+        public int ID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public CheeseType Type { get; set; }
-        public int ID { get; set; }
+
+        //for part 2
+        
+        public CheeseCategory Category { get; set; }
+        public int CheeseCategoryID { get; set; }
+
+        public IList<CheeseMenu> CheeseMenu { get; set; }
+
     }
 }
---
+++ src/CheeseMVC/Models/CheeseCategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheeseMVC.Models
+{
+    public class CheeseCategory
+    {
+        //In Models/, create a new model class named CheeseCategory. 
+        //Give it an ID property and a Name property that's a string.
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+        public IList<Cheese> Cheeses { get; set; }
+    }
+}
